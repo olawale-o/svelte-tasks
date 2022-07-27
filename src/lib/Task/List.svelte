5835acabb2f764e643ef6b5412af31615cb63acd
olawale-o <omoogunolawale@gmail.com>
2022-07-27 04:11:38 +0100

    Create TaskList card

---
+++ src/lib/Task/List.svelte
@@ -0,0 +1,39 @@
+<script>
+  import TaskCard from './index.svelte';
+  export let task;
+</script>
+<div class="task-container">
+  <button class="task-heading">
+    <span class="task-title">{task.title}</span>
+    <span class="task-title">{task.progressLevel}</span>
+  </button>
+  <div class="progress"></div>
+  <ul class="todo-tasks" id="todo-tasks">
+    {#each task.tasks as task}
+      <TaskCard {task} />
+    {/each}
+  </ul>
+</div>
+
+<style>
+  .task-heading {
+    background: #fff;
+    padding: 15px;
+    border: none;
+    outline: none;
+    display: block;
+    width: 100%;
+    text-align: left;
+    cursor: pointer;
+  }
+
+  .progress {
+    widows: 100%;
+    height: 1px;
+    background: green;
+  }
+
+  .task-title {
+    font-size: 2rem;
+  }
+</style>
