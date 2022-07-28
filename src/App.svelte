9996719168835f11c728935db70ab5c038d5925f
olawale-o <omoogunolawale@gmail.com>
2022-07-28 02:35:18 +0100

    Toggle one task or collapse all

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,89 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import TaskList from './lib/Task/List.svelte';
+  let currentActiveIndex = 0;
+  let tasks = [
+    {
+      id: 1,
+      title: "Deployment",
+      progressLevel: 50,
+      tasks: [
+        {
+          id: 1,
+          description: "Deployment Task One",
+          completed: true,
+          parentId: 1,
+        },
+        {
+          id: 2,
+          description: "Deployment Task Two",
+          completed: false,
+          parentId: 1,
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "Design",
+      progressLevel: 50,
+      tasks: [
+        {
+          id: 1,
+          description: "Design Task One",
+          completed: false,
+          parentId: 2,
+        },
+        {
+          id: 2,
+          description: "Design Task Two",
+          completed: true,
+          parentId: 2,
+        },
+      ],
+    }
+  ];
+
+  $: {
+    console.log(tasks);
+  }
+
+  /**
+   * @param {{ detail: { task: { id: number; description: string; completed: boolean; parentId: number }; }; }} e
+  */
+  function onTaskCheck(e) {
+    const oldTasks = [...tasks];
+    const parentTask = oldTasks.find((task) => task.id === e.detail.task.parentId);
+    const completeTasks = parentTask.tasks.filter((task) => task.completed === true).length;
+    parentTask.progressLevel = (completeTasks / parentTask.tasks.length) * 100;
+    tasks = oldTasks;
+  }
+
+  /**
+* @param {{ detail: { id: number; }; }} e
+*/
+  function toggleTask(e) {
+    if (currentActiveIndex === e.detail.id) {
+      currentActiveIndex = null;
+    } else {
+      currentActiveIndex = e.detail.id;
+    }
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class="todo-container">
+    <div class="todo-content">
+      {#each tasks as task, id}
+        <TaskList
+          on:checkedTask={onTaskCheck}
+          {task}
+          active={currentActiveIndex === id}
+          {id}
+          on:toggleTask={toggleTask}
+        />
+      {/each}
+    </div>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
--- src/lib/Task/List.svelte
+++ src/lib/Task/List.svelte
@@ -1,14 +1,23 @@
 <script>
+  import { createEventDispatcher } from 'svelte';
   import TaskCard from './index.svelte';
   export let task;
+  export let id;
+  export let active;
+  let open = false;
+  const dispatch = createEventDispatcher();
+  function toggleTask() {
+    open = !open;
+    dispatch('toggleTask',  { id });
+  }
 </script>
 <div class="task-container">
-  <button class="task-heading">
+  <button class="task-heading" on:click={toggleTask}>
     <span class="task-title">{task.title}</span>
     <span class="task-title">{task.progressLevel}</span>
   </button>
   <div class="progress"></div>
-  <ul class="todo-tasks" id="todo-tasks">
+  <ul class="todo-tasks" id="todo-tasks" class:open={active}>
     {#each task.tasks as task}
       <TaskCard on:checkedTask {task} />
     {/each}
@@ -35,5 +44,17 @@
 
   .task-title {
     font-size: 2rem;
+  }
+
+  .todo-tasks {
+    overflow-y: auto;
+    max-height: 0px;
+    padding: 0;
+    transition: all 300ms ease-in-out;
+  }
+
+  .todo-tasks.open {
+    max-height: initial;
+    transition: all 300ms ease-in-out;
   }
 </style>
