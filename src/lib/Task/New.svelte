8690a5de5a2e03a77a5da37a42e7114ce6bdf652
olawale-o <omoogunolawale@gmail.com>
2022-07-29 01:30:07 +0100

    Create AddTask component

---
+++ src/lib/Task/New.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+  const dispatch = createEventDispatcher();
+  let text;
+  export let parentId;
+  function addTask(e) {
+    if (e.key === 'Enter' || e.type === 'click') {
+      dispatch('addTask', {
+        parentId,
+        description: text,
+      });
+    }
+  }
+</script>
+<div class="input-field">
+  <input
+    type="text"
+    class="input"
+    on:keyup={addTask}
+    placeholder="Add to your list"
+    bind:value={text}
+  >
+  <span id="add"><i class='bx bx-subdirectory-left return' on:click={addTask}></i></span>
+</div>
