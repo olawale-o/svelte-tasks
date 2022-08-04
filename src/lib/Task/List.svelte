82571d6123a13072d47d2569349f346b679754df
olawale-o <omoogunolawale@gmail.com>
2022-08-04 08:00:44 +0100

    Update store after drag and drop ends

--- src/lib/Task/List.svelte
+++ src/lib/Task/List.svelte
@@ -1,15 +1,44 @@
 <script>
   import { createEventDispatcher } from 'svelte';
+  import { taskStore } from '../store/taskStore';
   import AddTask from './New.svelte';
   import TaskCard from './index.svelte';
   export let task;
   export let id;
   export let active;
   let open = false;
+  let dragItem = null;
+  let dragEnterItem = null;
   const dispatch = createEventDispatcher();
   function toggleTask() {
     open = !open;
     dispatch('toggleTask',  { id });
+  }
+
+  function start(e) {
+    dragItem = e.detail.start;
+  }
+
+  function enter(e) {
+    dragEnterItem = e.detail.enter;
+  }
+
+  $:tasks = $taskStore.tasks;
+  
+  function end() {
+    let _tasks = [...tasks];
+
+    let _parentTask = _tasks[id];
+
+    const dragItemContent = _parentTask.tasks.splice(dragItem, 1)[0];
+
+    _parentTask.tasks.splice(dragEnterItem, 0, dragItemContent);
+
+    _tasks[id] = _parentTask;
+
+    dragItem = null;
+    dragEnterItem = null;
+    taskStore.updateTasks(_tasks);
   }
 
 </script>
@@ -22,10 +51,13 @@
   </div>
   <ul class="todo-tasks" id="todo-tasks" class:open={active}>
     <AddTask parentId={task.id} />
-    {#each task.tasks as task, id}
+    {#each task.tasks as task, key}
       <TaskCard
         {task}
-        {id}
+        id={key}
+        on:start={start}
+        on:enter={enter}
+        on:end={end}
       />
     {/each}
   </ul>
