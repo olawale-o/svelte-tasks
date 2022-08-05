c870a9c91ca685b7e27ca3f2e56f38866c1dc6d1
olawale-o <omoogunolawale@gmail.com>
2022-08-05 07:20:04 +0100

    User can add main task

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
   import TaskList from './lib/Task/List.svelte';
   let currentActiveIndex = 0;
   $: {  }
+  let task = '';
 
   /**
 * @param {{ detail: { id: number; }; }} e
@@ -14,12 +15,28 @@
       currentActiveIndex = e.detail.id;
     }
   }
+  function addTask(e) {
+    if (e.key === 'Enter' || e.type === 'click') {
+      taskStore.addParentTask({ title: task, id: tasks.length + 1, progressLevel: 0, tasks: [] });
+      task = '';
+    }
+  }
   $: tasks = $taskStore.tasks;
 </script>
 
 <main>
   <div class="todo-container">
     <div class="todo-content">
+      <div class="input-field">
+        <input
+          type="text"
+          class="input"
+          placeholder="Add a task"
+          bind:value={task}
+          on:keyup={addTask}
+        >
+        <span id="add"><i class='bx bx-subdirectory-left return' on:click={addTask}></i></span>
+      </div>
       {#each tasks as task, id}
         <TaskList
           {task}
